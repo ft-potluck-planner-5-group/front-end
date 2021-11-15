04ceddefdf4995efa7543870999f3d6c62cae9e5
sooof <sooof@me.com>
2021-11-15 15:38:13 -0800

    home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
   <Router>
     <Header />
     <Switch>
-      
+      <Route exact path="/" component={Home}/>
     </Switch>
   </Router>
   );
