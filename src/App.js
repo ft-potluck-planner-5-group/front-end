db630cbbe24fd044d21ac56b32a0d6e7012c9bca
sooof <sooof@me.com>
2021-11-15 09:10:01 -0800

    make Header basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-// import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Front End</h1>
-    </div>
+  <Router>
+    <Header />
+    <Routes>
+      
+    </Routes>
+  </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import '../CSS/Header.css';
+
+const Header = () => {
+    return(
+        <nav className="nav-bar">
+            <div className="left-links">
+                <Link className="link" to='/'>Home</Link>
+                <Link className="link" to='/products'>Products</Link>
+            </div>
+            <div className="right-links">
+                <Link className="link" to='/login'>Login</Link>
+            </div>
+        </nav>
+    )
+}
+export default Header;
