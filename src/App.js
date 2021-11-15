fef4820f3fe37760703137cd60b3873bba9f65d3
sooof <sooof@me.com>
2021-11-15 11:39:31 -0800

    create Component/Products.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 import Header from './components/Header';
+// import Home from './components/Home';
+// import Login from './components/Login';
+import Products from './components/Products';
 
 function App() {
   return (
   <Router>
     <Header />
     <Routes>
-      
+        {/* <Route exact path="/" component={Home}/> */}
+        {/* <Route path="/login" component={Login} /> */}
+        <Route path='/products'component={Products}/> 
     </Routes>
   </Router>
   );
---
+++ src/components/Products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../CSS/products.css'
+
+const Products = () => {
+    return(
+        <>
+        </>
+    )
+}
+export default Products;
+
