6551ccdb3ad040780f84d646bd1848603ae1ac93
sooof <sooof@me.com>
2021-11-16 21:49:27 -0800

    add isLoggedIn

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom'
 import '../CSS/Header.css';
 
 const Header = () => {
+    const isLoggedIn = localStorage.getItem('token')
+    
     return(
         <nav className="nav-bar">
             <div className="left-links">
                 <Link className="link" to='/'>Home</Link>
-                <Link className="link" to='/products'>Products</Link>
+                {
+                    isLoggedIn ? <Link className="link" to='/products'>Products</Link> : ''
+                }
             </div>
             <div className="right-links">
                 <Link className="link" to='/signup'>Signup</Link>
                 <Link className="link" to='/login'>Login</Link>
-                <Link className="link" to='/logout'>Logout</Link>
+                {
+                    isLoggedIn ? <Link className="link" to='/logout'>Logout</Link> : ''
+                }
+                
             </div>
         </nav>
     )
