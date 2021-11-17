063df3cdf8e46b559eb9a625f33122cfda2ca8c2
sooof <sooof@me.com>
2021-11-16 18:58:30 -0800

    add Logout.js fix Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
 import '../CSS/Home.css';
+import { useHistory } from 'react-router-dom';
 
 const Home = () => {
+
+    const { push } = useHistory();
+    const handleClick = () => {
+        push('/login');
+    }
 
     return (
         <div className="home-div">
             <h1>Potluck Planner</h1>
-            <button className="home-btn">Login</button>
+            <button className="home-btn" onClick={handleClick} >Login</button>
         </div>
     )
 }
